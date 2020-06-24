9a7319cd9644a11cb6a769862866ca43f53e2d5f
alexhoerler <55326754+alexhoerler@users.noreply.github.com>
2020-06-24 18:08:16 -0400

    Add files via upload
    Add a conditional if packet is not IP/TCP

--- Parser.py
+++ Parser.py
@@ -7,30 +7,40 @@ if __name__ == "__main__":
     main(sys.argv[1:])
 
 
-# gets both the IP and TCP source and destination addresses
+# get both the IP and TCP source and destination addresses
 # If the frame doesn't contain a IP or TCP packet, print so
-# input is a string name of the packet capture
-def getIpTcpHead(packet_cap):
-    frame = open(packet_cap)
-    pcap = dpkt.pcap.Reader(frame)
-    for ts, buf in pcap:
+# packet_cap: a string name of the packet capture
+def get_eth_ip_tcp_head(packet_cap):
+    file = open(packet_cap)
+    pcap = dpkt.pcap.Reader(file)
+    for timestamp, buf in pcap:
         eth = dpkt.ethernet.Ethernet(buf)
 
+        # get the IP level data
         ip = eth.data
-        print('IP Packet: %s => %s' % (inet_to_str(ip.src), inet_to_str(ip.src)))
+        if not isinstance(ip, dpkt.ip.IP):
+            print("Not an instance of IPv4. Can't extract data.")
+            continue
+        else:
+            print("IP Packet: %s => %s" % (inet_to_str(ip.src), inet_to_str(ip.src)))
 
+        #get the TCP level data
         tcp = ip.data
-        print('TCP Packet: %s => %s' % (tcp.sport, tcp.dport))
+        if not isinstance(tcp, dpkt.tcp.TCP):
+            print("Not an instance. Can't extract data past the IP level.")
+            continue
+        else:
+            print("TCP Packet: %s => %s" % (tcp.sport, tcp.dport))
 
-    frame.close()
+    file.close()
 
 # gets the source and destination MAC addresses
-# input is a string name of the packet capture
-def getEthHead(packet_cap):
-    frame = open(packet_cap)
-    pcap = dpkt.pcap.Reader(frame)
-    for ts, buf in pcap:
+# packet_cap: a string name of the packet capture
+def get_eth_head(packet_cap):
+    file = open(packet_cap)
+    pcap = dpkt.pcap.Reader(file)
+    for timestamp, buf in pcap:
         eth = dpkt.ethernet.Ethernet(buf)
-        print('Ethernet Frame: %s => %s' % (mac_addr(eth.src), mac_addr(eth.dst)))
+        print("Ethernet Frame: %s => %s" % (mac_addr(eth.src), mac_addr(eth.dst)))
 
-    frame.close()
+    file.close()
